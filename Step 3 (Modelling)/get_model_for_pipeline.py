2fb8de7da578d51bb03334ced859ce9a4571d7ea
Souljah_Pc <utkucan06.45@gmail.com>
2024-08-01 15:29:42 +0300

    --

--- Step 3 (Modelling)/get_model_for_pipeline.py
+++ Step 3 (Modelling)/get_model_for_pipeline.py
@@ -6,7 +6,7 @@ import joblib
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
@@ -17,10 +17,12 @@ pd.options.display.float_format = '{:.4f}'.format
 
 car_df = pd.read_csv('Step 2 (Prepare Data)/car_df_for_model.csv')
 
+# model ile seri sütunlarının isimlerini değiştir:
+car_df['model'], car_df['seri'] = car_df['seri'], car_df['model']
+
 # Sütunları sırala:
 column_order = ['marka', 'model', 'seri', 'yil', 'kilometre', 'vites_tipi', 'yakit_tipi', 'kasa_tipi',
                 'renk', 'boya', 'degisen', 'Fiyat']
-
 car_df = car_df[column_order]
 
 # Özellik ve hedef değişkenlerini ayır:
@@ -126,24 +128,78 @@ preprocessor = ColumnTransformer(
 model_pipeline = Pipeline(
     [
         ('preprocessor', preprocessor),
-        ('model', RandomForestRegressor(verbose = 1, random_state = 42))
+        ('model', RandomForestRegressor(verbose = 1, random_state = 42, n_jobs = -1))
         ]
     )
 
 # Veri setini ayır:
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
 
-# Modeli eğit
+# Basit parametrelerle GridSearchCV hiperparametre optimizasyonu:
+param_grid = {
+    'model__n_estimators':      [100, 200],  # default: 100
+    'model__max_depth':         [None, 10, 20],  # default: None
+    'model__min_samples_split': [2, 5],  # default: 2
+    'model__min_samples_leaf':  [1, 2],  # default: 1
+    'model__bootstrap':         [True, False]
+    }
+
+grid_search = GridSearchCV(
+    estimator = model_pipeline,
+    param_grid = param_grid,
+    cv = 3,
+    n_jobs = 8,
+    verbose = 2,
+    scoring = 'neg_mean_squared_error'
+    )
+
+# Modeli eğit:
+grid_search.fit(X_train, y_train)
+
+# En iyi modeli al:
+randomforrestregressor = grid_search.best_estimator_
+# En iyi parametreleri al:
+grid_search.best_params_
+
+# Tahmin yap:
+y_pred_train = randomforrestregressor.predict(X_train)
+y_pred_test = randomforrestregressor.predict(X_test)
+
+# Model performansını değerlendir:
+print('Train RMSE:', mean_squared_error(y_train, y_pred_train, squared = False))  # 35129
+print('Test RMSE:', mean_squared_error(y_test, y_pred_test, squared = False))  # 75239
+print('Best Parameters:', grid_search.best_params_)
+# {'model__bootstrap': True,
+#  'model__max_depth': None,
+#  'model__min_samples_leaf': 1,
+#  'model__min_samples_split': 5,
+#  'model__n_estimators': 200}
+
+# Model ve scaler değerlerini kaydet:
+joblib.dump(randomforrestregressor, 'Step 3 (Modelling)/randomforrestregressor_model.pkl')
+joblib.dump(preprocessor, 'Step 3 (Modelling)/preprocessor.pkl')  # Buna gerek yok.
+
+
+# En iyi hiperparametrelerle bir model eğit:
+model_pipeline = Pipeline(
+    [
+        ('preprocessor', preprocessor),
+        ('model', RandomForestRegressor(bootstrap = True,
+                                        max_depth = None,
+                                        min_samples_leaf = 1,
+                                        min_samples_split = 5,
+                                        n_estimators = 200,
+                                        verbose = 2,
+                                        n_jobs = -1,
+                                        random_state = 42))
+        ]
+    )
+
 model_pipeline.fit(X_train, y_train)
 
-# Tahmin yap
+# Tahmin yap:
 y_pred_train = model_pipeline.predict(X_train)
 y_pred_test = model_pipeline.predict(X_test)
 
-# Model performansını değerlendir
-print('Train RMSE:', mean_squared_error(y_train, y_pred_train, squared = False))
-print('Test RMSE:', mean_squared_error(y_test, y_pred_test, squared = False))
 
-# Model ve scaler değerlerini kaydet:
-joblib.dump(model_pipeline, 'Step 3 (Modelling)/pipeline_model.pkl')
-joblib.dump(preprocessor, 'Step 3 (Modelling)/preprocessor.pkl')  # Bu satıra gerek yok.
+plot_grouped_importance()
